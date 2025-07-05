3e1a1d9a8c93aa9956f238fe60b5f399fa3d1a7e
Zerazun <zerazun@gmail.com>
2025-07-06 01:29:02 +0300

    Update menu.c

--- example_menu/menu.c
+++ example_menu/menu.c
@@ -8,8 +8,8 @@ void OnMenuOpen()
 
   // Set checkboxes states
   // Меняем состояние чекбоксов
-  SendMessage(&GameInterface, "lslll", MSG_INTERFACE_MSG_TO_NODE, "EXAMPLE_CHECKBOX1", 2, 1, 1);
-  SendMessage(&GameInterface, "lslll", MSG_INTERFACE_MSG_TO_NODE, "EXAMPLE_CHECKBOX2", 2, 1, 0);
+  SendMessage(&GameInterface, "lslll", MSG_INTERFACE_MSG_TO_NODE, "EXAMPLE_CHECKBOX1", 2, 1, Example_OptionEnabled("exampleOption1"));
+  SendMessage(&GameInterface, "lslll", MSG_INTERFACE_MSG_TO_NODE, "EXAMPLE_CHECKBOX2", 2, 1, Example_OptionEnabled("exampleOption2"));
 
   SetEventHandler("ievnt_command","ProcCommand",0);
 }
@@ -28,8 +28,26 @@ void ProcCommand()
   string comName = GetEventData();
   string nodName = GetEventData();
 
+  if (comName != "click") return;
+
   switch(nodName)
   {
-    Log_Info("nodNm: " + nodName + " comName " + comName);
+    Log_Info("nodName: " + nodName + " comName: " + comName);
+    case "EXAMPLE_CHECKBOX1": Example_OptionSwitch("exampleOption1"); break;
+    case "EXAMPLE_CHECKBOX2": Example_OptionSwitch("exampleOption2"); break;
   }
+}
+
+void Example_OptionSwitch(string option)
+{
+  bool state = Example_OptionEnabled(option);
+  InterfaceStates.ExampleOptions.(option) = !state;  //switch
+  Log_Info(option + ": " + InterfaceStates.ExampleOptions.(option))
+}
+
+bool Example_OptionEnabled(string option)
+{
+  if (CheckAttribute(&InterfaceStates, "ExampleOptions." + option)) return sti(InterfaceStates.ExampleOptions.(option)) != 0;  //return current value
+  if (option == "exampleOption2") return false;  //default false
+  return true;  //rest default true
 }
